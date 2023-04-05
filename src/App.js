acc15604fc9d2504faeaea9e77a8f1d6170dedfe
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-04-05 12:10:43 -0400

    fetch activity api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [activity, setActivity] = useState("");
+
+  useEffect(() => {
+    getActivity();
+  }, []);
+
+  const getActivity = async () => {
+    const url = `http://www.boredapi.com/api/activity/`;
+    const res = await fetch(url);
+    const data = await res.json();
+    setActivity(data);
+    console.log(data);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>{activity.activity}</h3>
     </div>
   );
 }
