bde1b13ccb1799716984dd83413b9e6dcd71b4ff
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-04-10 15:44:04 -0400

    add styling to page

--- src/App.js
+++ src/App.js
@@ -21,6 +21,48 @@ function App() {
     <div className="App">
       <h3>{activity.activity}</h3>
       <button onClick={getActivity}>new activity</button>
+
+      <div className="container">
+        <svg
+          viewBox="0 0 500 500"
+          preserveAspectRatio="xMinYMin meet"
+          className="svgOne"
+        >
+          <path
+            className="pathOne"
+            d="M0, 100 C150, 200 350,
+                0 500, 100 L500, 00 L0, 0 Z"
+          ></path>
+        </svg>
+      </div>
+
+      <div className="container">
+        <svg
+          className="svgTwo"
+          viewBox="0 0 500 500"
+          preserveAspectRatio="xMinYMin meet"
+        >
+          <path
+            className="pathTwo"
+            d="M0, 80 C300, 0 400,
+                300 500, 50 L500, 00 L0, 0 Z"
+          ></path>
+        </svg>
+      </div>
+
+      <div className="container">
+        <svg
+          className="svgThree"
+          viewBox="0 0 500 500"
+          preserveAspectRatio="xMinYMin meet"
+        >
+          <path
+            className="pathThree"
+            d="M0, 100 C150, 300 350,
+                0 500, 100 L500, 00 L0, 0 Z"
+          ></path>
+        </svg>
+      </div>
     </div>
   );
 }
