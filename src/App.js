ccbf875a3fb1210c36ffed81e151da3a72a72678
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-04-11 10:31:27 -0400

    add gsap to page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,51 @@
 import "./App.css";
 import { useEffect, useState } from "react";
+import { gsap } from "gsap";
 import video from "./video.mp4";
+import React from "react";
+
+const { useLayoutEffect, useRef } = React;
 
 function App() {
+  const app = useRef();
+  const button = useRef();
+
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      gsap.from(".svgOne", {
+        duration: 2,
+        y: 100,
+        yPercent: -100,
+      });
+      gsap.from(".svgTwo", {
+        duration: 2,
+        y: 200,
+        yPercent: -100,
+        delay: 0.7,
+      });
+      gsap.from(".svgThree", {
+        duration: 2,
+        y: 300,
+        yPercent: -100,
+        delay: 1,
+        ease: "bounce.out",
+      });
+      gsap.from(".title", {
+        duration: 1,
+        scale: 0.5,
+        opacity: 0,
+        delay: 0.5,
+      });
+      gsap.from(button.current, {
+        duration: 1,
+        scale: 0.5,
+        opacity: 0,
+        ease: "elastic",
+      });
+    }, app);
+    return () => ctx.revert();
+  }, []);
+
   const [activity, setActivity] = useState("");
 
   useEffect(() => {
@@ -19,13 +62,13 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <div className="App" ref={app}>
       <video autoPlay muted loop>
         <source src={video} type="video/mp4" />
       </video>
       <div className="buttonContainer">
         <h3 className="title">{activity.activity}</h3>
-        <button className="button" onClick={getActivity}>
+        <button ref={button} className="button" onClick={getActivity}>
           new activity
         </button>
       </div>
@@ -43,7 +86,6 @@ function App() {
             ></path>
           </svg>
         </div>
-
         <div className="container">
           <svg
             className="svgTwo"
@@ -57,7 +99,6 @@ function App() {
             ></path>
           </svg>
         </div>
-
         <div className="container">
           <svg
             className="svgThree"
